7abe80692147efd7aca4caf9024962d9156f32d6
alghozaliii <alghozali4331@student.uns.ac.id>
2025-03-04 01:07:57 +0700

    landingwooo

---
+++ resources/js/Pages/LandingPage.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="flex overflow-hidden flex-col items-center px-20 pt-7 pb-32 bg-white max-md:px-5 max-md:pb-24">
+    <NavigationHeader />
+    <HeroSection />
+    <FeaturesSection />
+  </div>
+</template>
+
+<script>
+import NavigationHeader from "../Components/NavigationHeader.vue";
+import HeroSection from "../Components/HeroSection.vue";
+import FeaturesSection from "../Components/FeaturesSection.vue";
+
+export default {
+  name: "LandingPage",
+  components: {
+    NavigationHeader,
+    HeroSection,
+    FeaturesSection,
+  },
+};
+</script>
